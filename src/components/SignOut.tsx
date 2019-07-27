1707d2530bd811d2c7550e3a7249c358da41034f
Artem Golendukhin <artem.golendukhin@gmail.com>
2019-07-27 16:02:57 +0300

    fix sign out for mobile

--- src/components/SignOut.tsx
+++ src/components/SignOut.tsx
@@ -5,6 +5,11 @@ const SignOut = styled(Button)`
   position: absolute;
   top: 40px;
   right: 40px;
+  /* Portrait iPhones */
+  @media only screen and (min-device-width: 320px) and (max-device-width: 480px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait) {
+    top: 10px;
+    right: 10px;
+  }
 `
 
 export default SignOut
